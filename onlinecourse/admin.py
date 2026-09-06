cb3e9ae9ef33931e930d4d2d462a7925c93224dc
Prakruti-design <pipaliyaprakruti@gmail.com>
2026-09-06 17:40:19 -0400

    Implement exam submission and result evaluation

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -21,7 +21,7 @@ class QuestionInline(admin.StackedInline):
 
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
+    inlines = [LessonInline, QuestionInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
